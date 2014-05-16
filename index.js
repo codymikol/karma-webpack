1a8e6410995f778f63511a5e4d82b6ee150e1403
Lendar <xeldim@gmail.com>
2014-05-16 20:36:37 +0400

    fixed karma's autoWatch (closes #9)
    Closes 3 bugs using 2 hacks.
    
    #### 1. Fix early restart on test change
    
    Test scenario
    
    1. Change test file to `throw new Error('foo')`.
    2. Wait for recompilation.
    3. Change test file to `throw new Error('bar')`.
    
    Expected: test is failed with `bar` message.
    Result in v1.1 (afaa7b3f7117c8ebce5dd07d0c9b8dba3250c313): test is failed with `foo` message.
    
    #### 2. Fix ignoring of test dependencies change
    
    Test scenario
    
    1. Change test dependency
    2. Wait for recompilation
    
    Expected: test is run.
    Result in v1.1 (afaa7b3f7117c8ebce5dd07d0c9b8dba3250c313): nothing.
    
    #### 3. Fix browser cache
    
    Test scenario
    
    1. Change test dependency
    2. Wait for recompilation
    3. Karma serves file with the same sha
    
    Expected: browser gets the new file.
    Result in v1.1 (afaa7b3f7117c8ebce5dd07d0c9b8dba3250c313): browser uses cached version.
    Workarounds: bust browser cache manually / re-open browser.

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
+var fs = require("fs");
+var crypto = require("crypto");
 var path = require("path");
 var async = require("async");
 var webpackDevServer = require("webpack-dev-server");
 var webpack = require("webpack");
 var SingleEntryDependency = require("webpack/lib/dependencies/SingleEntryDependency");
 
-function Plugin(/* config.port */karmaPort, /* config.hostname */hostname, /* config.webpackPort */port, /* config.webpack */webpackOptions, /* config.webpackServer */webpackServerOptions, /* config.basePath */basePath) {
+function Plugin(/* config.port */karmaPort, /* config.hostname */hostname, /* config.webpackPort */port, /* config.webpack */webpackOptions, /* config.webpackServer */webpackServerOptions, /* config.basePath */basePath, fileList) {
 	if(!port) port = karmaPort + 1;
 	if(!hostname) hostname = "localhost";
 	if(!webpackOptions) webpackOptions = {};
 	if(!webpackOptions.output) webpackOptions.output = {};
 	if(!webpackServerOptions) webpackServerOptions = {};
-	
+
 	this.files = [];
 	this.basePath = basePath;
 	this.waiting = [];
-	
+	this.fileList = fileList;
+
 	webpackOptions.output.path = "/";
 	webpackOptions.output.filename = "_js/[name]";
 	webpackOptions.output.chunkFilename = "_js/[id].chunk.js";
 	webpackOptions.output.publicPath = "http://" + hostname + ":" + port + "/";
-	
+
 	var compiler = webpack(webpackOptions);
 	compiler.plugin("compilation", function(compilation, params) {
 		compilation.dependencyFactories.set(SingleEntryDependency, params.normalModuleFactory);
 	});
-	compiler.plugin("done", function() {
+	compiler.plugin("done", function(stats) {
+		// If karma is already in preprocessing phase, no need to trigger karma run.
+		if(!this.karmaWaitsForPreprocessing) {
+			// If file required in tests is changed, webpack compilation is done silently for karma.
+			// Fix this by emulating test file change.
+			this.notifyKarmaAboutChanges(stats);
+		}
+
 		if(this.waiting) {
 			var w = this.waiting;
 			this.waiting = null;
@@ -41,11 +51,47 @@ function Plugin(/* config.port */karmaPort, /* config.hostname */hostname, /* co
 	server.listen(port, hostname);
 }
 
+Plugin.prototype.notifyKarmaAboutChanges = function(stats) {
+	// Find recently recompiled files.
+	var changedAssets = stats.toJson().assets.filter(function (asset) {
+		return asset.emitted;
+	}).map(function(asset) {
+		return asset.name;
+	});
+
+	// Pick files watched by karma among them.
+	var changedTests = this.files.filter(function (file) {
+		var assetName = "_js/" + path.relative(this.basePath, file).replace(/\\/g, "/");
+		return changedAssets.indexOf(assetName) !== -1;
+	}.bind(this));
+
+	// Make karma to run preprocessors on changed tests.
+	changedTests.forEach(function(changedTestFile) {
+		this.fileList.buckets.forEach(function(bucket) {
+			bucket.forEach(function(watchedFile) {
+				if (watchedFile.originalPath === changedTestFile) {
+					this.changeKarmaFile(watchedFile);
+				}
+			}.bind(this));
+		}.bind(this));
+	}.bind(this));
+}
+
 Plugin.prototype.addFile = function(entry) {
 	if(this.files.indexOf(entry) >= 0) return;
 	this.files.push(entry);
 	if(!this.waiting) this.waiting = [];
 	this.server.invalidate();
+};
+
+Plugin.prototype.changeKarmaFile = function(watchedFile) {
+	// Hack: utimes
+	//
+	// Karma v0.12 will do fs.stat to compare it with watchedFile.mtime.
+	// https://github.com/karma-runner/karma/blob/v0.12.16/lib/file_list.js#L362
+	// Not doing utimes() causes karma to ignore changeFile event.
+	fs.utimesSync(watchedFile.originalPath, new Date(), new Date());
+	this.fileList.changeFile(watchedFile.originalPath);
 };
 
 Plugin.prototype.make = function(compilation, callback) {
@@ -59,16 +105,28 @@ Plugin.prototype.readFile = function(file, callback) {
 	function doRead() {
 		server.middleware.fileSystem.readFile("/_js/"+file.replace(/\\/g, "/"), callback);
 	}
-	if(!this.waiting) doRead();
-	else this.waiting.push(doRead);
+	if(!this.waiting) this.waiting = [];
+	this.waiting.push(doRead);
 }
 
+var sha1 = function(data) {
+  var hash = crypto.createHash('sha1');
+  hash.update(data);
+  return hash.digest('hex');
+};
 
 function createPreprocesor(/* config.basePath */basePath, webpackPlugin, logger, config) {
 	var log = logger.create("preprocessor.webpack");
 	return function(content, file, done) {
+		webpackPlugin.karmaWaitsForPreprocessing = true;
 		webpackPlugin.addFile(file.path);
 		webpackPlugin.readFile(path.relative(basePath, file.path), function(err, content) {
+			webpackPlugin.karmaWaitsForPreprocessing = false;
+			// Hack: file.sha
+			//
+			// Karma v0.12 assumes preprocessing is idempotent - always returns the same result on the same input.
+			// That's not true with karma-webpack. Not setting file.sha causes browser to use a cached file.
+			file.sha = sha1(content);
 			done(err, content && content.toString("utf-8"));
 		});
 	};
