d06713b87858e02a689471a7e981e2bd095f7842
Tobias Koppers <tobias.koppers@googlemail.com>
2015-01-06 22:17:03 +0100

    more test cases, fixed a bit of stuff #34

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ var webpack = require("webpack");
 var SingleEntryDependency = require("webpack/lib/dependencies/SingleEntryDependency");
 
 function Plugin(
-			/* config.webpack */webpackOptions, /* config.webpackMiddleware */webpackMiddlewareOptions,
+			/* config.webpack */webpackOptions,
+			/* config.webpackServer */webpackServerOptions,
+			/* config.webpackMiddleware */webpackMiddlewareOptions,
 			/* config.basePath */basePath,
 			/* config.files */files,
 			/* config.frameworks */frameworks,
+			fileList,
 			customFileHandlers,
 			emitter) {
 	if(!webpackOptions) webpackOptions = {};
 
-	if(!webpackMiddlewareOptions) webpackMiddlewareOptions = {};
+	if(!webpackMiddlewareOptions) webpackMiddlewareOptions = webpackServerOptions || {};
 
 	var applyOptions = Array.isArray(webpackOptions) ? webpackOptions : [webpackOptions];
 	var includeIndex = applyOptions.length > 1;
@@ -28,18 +31,19 @@ function Plugin(
 		// This is due to the way that the dev server exposes commonPath for build output.
 		var indexPath = includeIndex ? index + "/" : "";
 
-		// Must have the common _js prefix on path here to avoid
+		// Must have the common _karma_webpack_ prefix on path here to avoid
 		// https://github.com/webpack/webpack/issues/645
-		webpackOptions.output.path = "/_js/" + indexPath;
-		webpackOptions.output.publicPath = "/_js/";
+		webpackOptions.output.path = "/_karma_webpack_/" + indexPath;
+		webpackOptions.output.publicPath = "/_karma_webpack_/" + indexPath + "/";
 		webpackOptions.output.filename = "[name]";
 		if(includeIndex)
 			webpackOptions.output.jsonpFunction = "webpackJsonp" + index;
 		webpackOptions.output.chunkFilename = "[id].chunk.js";
 	});
 
+	this.fileList = fileList;
 	this.wrapMocha = frameworks.indexOf('mocha') >= 0 && includeIndex;
-	this.includeIndex = includeIndex;
+	this.optionsCount = applyOptions.length;
 	this.files = [];
 	this.basePath = basePath;
 	this.waiting = [];
@@ -56,17 +60,20 @@ function Plugin(
 	compiler.plugin("done", function(stats) {
 		var applyStats = Array.isArray(stats.stats) ? stats.stats : [stats];
 		var assets = [];
+		var noAssets = false;
 		applyStats.forEach(function(stats) {
 			stats = stats.toJson();
 
 			assets.push.apply(assets, stats.assets);
+			if(stats.assets.length === 0)
+				noAssets = true;
 		});
 
 		if(!this.waiting || this.waiting.length === 0) {
 			this.notifyKarmaAboutChanges();
 		}
 
-		if(this.waiting && assets.length > 0) {
+		if(this.waiting && !noAssets) {
 			var w = this.waiting;
 			this.waiting = null;
 			w.forEach(function(cb) {
@@ -78,11 +85,11 @@ function Plugin(
 		if(!this.waiting) this.waiting = [];
 	}.bind(this));
 
-	webpackMiddlewareOptions.publicPath = "/_js/";
+	webpackMiddlewareOptions.publicPath = "/_karma_webpack_/";
 	var middleware = this.middleware = new webpackDevMiddleware(compiler, webpackMiddlewareOptions);
 
 	customFileHandlers.push({
-		urlRegex: /\/_js\/.*/,
+		urlRegex: /^\/_karma_webpack_\/.*/,
 		handler: function(req, res) {
 			middleware(req, res, function() {
 				res.statusCode = 404;
@@ -110,7 +117,7 @@ Plugin.prototype.addFile = function(entry) {
 
 Plugin.prototype.make = function(compilation, callback) {
 	async.forEach(this.files.slice(), function(file, callback) {
-		var entry = "./" + path.relative(this.basePath, file);
+		var entry = file;
 		if (this.wrapMocha) {
 			entry = require.resolve("./mocha-env-loader") + "!" + entry;
 		}
@@ -131,10 +138,24 @@ Plugin.prototype.make = function(compilation, callback) {
 
 Plugin.prototype.readFile = function(file, callback) {
 	var middleware = this.middleware;
-	var includeIndex = this.includeIndex;
+	var optionsCount = this.optionsCount;
 
 	function doRead() {
-		middleware.fileSystem.readFile("/_js/" + (includeIndex ? "0/" : "") + file.replace(/\\/g, "/"), callback);
+		if(optionsCount > 1) {
+			async.times(optionsCount, function(idx, callback) {
+				middleware.fileSystem.readFile("/_karma_webpack_/" + idx + "/" + file.replace(/\\/g, "/"), callback);
+			}, function(err, contents) {
+				if(err) return callback(err);
+				contents = contents.reduce(function(arr, x) {
+					if(!arr) return [x];
+					arr.push(new Buffer("\n"), x);
+					return arr;
+				}, null);
+				callback(null, Buffer.concat(contents));
+			});
+		} else {
+			middleware.fileSystem.readFile("/_karma_webpack_/" + file.replace(/\\/g, "/"), callback);
+		}
 	}
 	if(!this.waiting)
 		doRead();
@@ -144,9 +165,7 @@ Plugin.prototype.readFile = function(file, callback) {
 		this.waiting.push(process.nextTick.bind(process, this.readFile.bind(this, file, callback)));
 };
 
-function createPreprocesor(/* config.basePath */basePath, webpackPlugin, fileList) {
-	webpackPlugin.fileList = fileList;
-
+function createPreprocesor(/* config.basePath */basePath, webpackPlugin) {
 	return function(content, file, done) {
 
 		if (webpackPlugin.addFile(file.path)) {
