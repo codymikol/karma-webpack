a2ecd6970df20c6754dc847fbd71635ba4964467
Will Farley <willf@hzdg.com>
2016-07-24 11:57:52 -0400

    :shirt: Appease the linter

--- example/karma.conf.js
+++ example/karma.conf.js
@@ -12,96 +12,96 @@ var configSettings = {
 };
 
 module.exports = function(config) {
-  config.set({
+	config.set({
 
-	// base path, that will be used to resolve files and exclude
-	basePath: '',
-
-
-	// frameworks to use
-	frameworks: ['mocha'],
+		// base path, that will be used to resolve files and exclude
+		basePath: '',
 
 
-	// list of files / patterns to load in the browser
-	files: [
-	  'test/index.js'
-	],
+		// frameworks to use
+		frameworks: ['mocha'],
 
 
-	// list of preprocessors
-	preprocessors: {
-	  'test/*': ['webpack']
-	},
+		// list of files / patterns to load in the browser
+		files: [
+			'test/index.js'
+		],
 
 
-	webpack: {
-		resolve: {
-			extensions: ["", ".js", ".coffee"]
+		// list of preprocessors
+		preprocessors: {
+			'test/*': ['webpack']
 		},
-		module: {
-			loaders: [
-				{ test: /\.coffee$/, loader: "coffee-loader" }
-			]
-		}
-	},
 
 
-	webpackMiddleware: {
-		stats: {
-			colors: true
-		}
-	},
+		webpack: {
+			resolve: {
+				extensions: ["", ".js", ".coffee"]
+			},
+			module: {
+				loaders: [
+					{ test: /\.coffee$/, loader: "coffee-loader" }
+				]
+			}
+		},
 
 
-	// test results reporter to use
-	// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-	reporters: ['spec'],
+		webpackMiddleware: {
+			stats: {
+				colors: true
+			}
+		},
 
 
-	// web server port
-	port: 9876,
+		// test results reporter to use
+		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+		reporters: ['spec'],
 
 
-	// enable / disable colors in the output (reporters and logs)
-	colors: true,
+		// web server port
+		port: 9876,
 
 
-	// level of logging
-	// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-	logLevel: config.LOG_INFO,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
 
-	// enable / disable watching file and executing tests whenever any file changes
-	autoWatch: true,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-	// Start these browsers, currently available:
-	// - Chrome
-	// - ChromeCanary
-	// - Firefox
-	// - Opera (has to be installed with `npm install karma-opera-launcher`)
-	// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-	// - PhantomJS
-	// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-	browsers: ['Chrome'],
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
 
-	// If browser does not capture in given timeout [ms], kill it
-	captureTimeout: 60000,
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera (has to be installed with `npm install karma-opera-launcher`)
+		// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+		// - PhantomJS
+		// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+		browsers: ['Chrome'],
 
 
-	// Continuous Integration mode
-	// if true, it capture browsers, run tests and exit
-	singleRun: false,
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 60000,
 
 
-	// List plugins explicitly, since autoloading karma-webpack
-	// won't work here
-	plugins: [
-		require("karma-mocha"),
-		require("karma-spec-reporter"),
-		require("karma-chrome-launcher"),
-		require("../")
-	]
-  });
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false,
+
+
+		// List plugins explicitly, since autoloading karma-webpack
+		// won't work here
+		plugins: [
+			require("karma-mocha"),
+			require("karma-spec-reporter"),
+			require("karma-chrome-launcher"),
+			require("../")
+		]
+	});
 };
