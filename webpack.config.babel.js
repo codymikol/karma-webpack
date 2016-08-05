e7d16e34cf0393488af34b8e536431cc6301d0fa
Mika Kalathil <mika@kalathil.me>
2016-08-04 23:45:42 -0700

    chore(build): create seperate builds for dev and dist (#161)
    Seperate builds out, dist minifies and remove comments where as dev is pretty raw

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,13 @@
 import path from 'path'
+import {optimize} from 'webpack'
 import {name, dependencies, devDependencies} from './package.json'
+
+const {UglifyJsPlugin} = optimize
 
 const rootPath = (nPath) => path.resolve(__dirname, nPath)
 const BUILD_PATH = './dist'
 const NODE_MODULES = rootPath('node_modules')
+const IS_BUILD = process.env.NODE_ENV === 'production'
 
 const CONFIG = {
   entry: './src/karma-webpack.js',
@@ -31,5 +35,13 @@ const CONFIG = {
   externals: Object.keys(dependencies)
     .concat(Object.keys(devDependencies))
 }
+
+if (IS_BUILD)
+  CONFIG.plugins = [
+    new UglifyJsPlugin({
+      comments: false,
+      sourceMap: false
+    })
+  ]
 
 export default CONFIG
