4b652695602c0c97057c46b3d9ce0415e39b5529
Joshua Wiens <joshuaw@easymetrics.com>
2017-01-15 17:57:45 -0600

    style(eslint): update for lint config

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,7 @@
 import {assert} from 'chai'
 
-describe('Webpack Karma', function () {
-  it('is true', function () {
+describe('Webpack Karma', function() {
+  it('is true', function() {
     assert.isTrue(true)
   })
 })
