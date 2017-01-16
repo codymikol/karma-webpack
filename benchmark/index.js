cb532eafb28212ac1e557018265ba0916a133059
Joshua Wiens <wiens.joshua@gmail.com>
2017-01-15 18:59:31 -0600

    chore(package): update devDeps & travis validations (#204)
    * chore(package): update devDeps
    
    * chore(lint): fix eslint config
    
    * chore(release): remove legacy release scripts
    
    * style(eslint): update for lint config
    
    * ci(Travis): update travis matrix and scripts
    
    * ci(Travis): use yarn
    
    * ci(Travis): don't check yarn version
    
    * ci(Travis): fix webpack install
    
    * ci(Travis): Add webpack 1.x / Node 7  to travis matrix
    
    * chore(package): update version and changelog for 2.x
    
    * ci(Travis): drop node7 from build matrix

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,24 +1,25 @@
-import {Suite} from 'benchmark'
+import benchmark from 'benchmark'
 
-const a = 1
-const b = 2
+const suite = new benchmark.Suite
 
-let result = 0
+const a = 1000
+const b = 2000
 
-Suite
-  .add('sum module addition with 2 numbers', function () {
+let result = 0 // eslint-disable-line no-unused-vars
+
+suite
+  .add('Benchmark Description 1', () => {
     result = a + b
   })
-  .add('sum module addition with 2 numbers', function () {
-    result = a + b
+  .add('Benchmark Description 2', () => {
+    result = ((a * 1000) / 1000) + ((b * 1000) / 1000)
   })
   // add listeners
-  .on('cycle', function (event) {
-    console.log(String(event.target))
+  .on('cycle', event => {
+    console.log(String(event.target)) // eslint-disable-line no-console
   })
-  .on('complete', function () {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  .on('complete', function() {
+    console.log(`Fastest is ${this.filter('fastest').map('name')}`) // eslint-disable-line no-console
   })
   // run async
-  .run({ 'async': true })
-
+  .run({async: true})
