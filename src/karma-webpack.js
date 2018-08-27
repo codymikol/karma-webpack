bac4676a64cc50314b2fb32d8401a7f2e2ff72e9
Pat Herlihy <pat841@bu.edu>
2018-08-27 09:06:09 -0400

    fix(karma-webpack): dont include the os.tmpdir in the webpack public path

--- src/karma-webpack.js
+++ src/karma-webpack.js
@@ -79,7 +79,7 @@ function Plugin(
       '/'
     );
     webpackOptions.output.publicPath = path.join(
-      os.tmpdir(),
+      '/',
       '_karma_webpack_',
       publicPath,
       '/'
@@ -209,7 +209,7 @@ function Plugin(
   compiler.hooks.invalid.tap(this.plugin, invalid.bind(this));
 
   webpackMiddlewareOptions.publicPath = path.join(
-    os.tmpdir(),
+    '/',
     '_karma_webpack_',
     '/'
   );
