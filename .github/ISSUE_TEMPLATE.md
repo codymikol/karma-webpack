2a4d3bbaa4d0daf13e4dbb6af45bd0703407d7db
Ryan Clark <ryan@ryanclark.me>
2018-09-01 03:40:51 +0100

    refactor(package): apply `webpack-defaults` (#340)

--- .github/ISSUE_TEMPLATE.md
+++ .github/ISSUE_TEMPLATE.md
@@ -1,42 +1,16 @@
-## **BEFORE YOU SUBMIT** please read the following:
 <!--
-If you have a support request or question please submit them to
-[StackOverflow](https://stackoverflow.com/questions/tagged/webpack) using the tag
-`[webpack]` or
-the [webpack Gitter](https://gitter.im/webpack/webpack). Future support requests will be closed.
+  👉🏽 Need support, advice, or help? Don't open an issue!
+  Head to StackOverflow or https://gitter.im/webpack/webpack.
+
+  Hey there!
+
+  You arrived at this template because you felt none of the other options
+  matched the kind of issue you'd like to report. Please use this opportunity to
+  tell us about your particular type of issue so we can try to accomodate
+  similar issues in the future.
+
+  PLEASE do note, if you're using this to report an issue already covered by the
+  existing template types, your issue may be closed as invalid. Our issue
+  templates contain fields that help us help you, and without that important
+  info, we might as well be ice-skating uphill, carrying a wooly mammoth.
 -->
-
-**I'm submitting a bug report**
-**I'm submitting a feature request**
-**I'm submitting a support request** => Please do not submit support request here, see note at the top of this template.
-
-
-**Webpack version:**
-1.10.x/2.x
-
-**Webpack Karma version**:
-1.7.x
-
-**Karma version**:
-0.13.x/1.x
-
-
-**Please tell us about your environment:**
-OSX 10.x / Linux / Windows 10
-
-**Browser:** [Phantom XX | Chrome XX | Firefox XX | IE XX | Safari XX | Other XX]
-
-**Current behavior:**
-
-
-**Expected/desired behavior:**
-
-
-* **If the current behavior is a bug, please provide the steps to reproduce and if possible a minimal demo of the problem along with a gist/jsbin of your webpack configuration.**
-
-
-* **What is the expected behavior?**
-
-
-* **What is the motivation / use case for changing the behavior?**
-
--- src/mocha-env-loader.js
+++ src/mocha-env-loader.js
@@ -1,38 +1,41 @@
-var sourceMap = require('source-map')
-var loaderUtils = require('loader-utils')
+const sourceMap = require('source-map');
+const loaderUtils = require('loader-utils');
 
-var SourceNode = sourceMap.SourceNode
-var SourceMapConsumer = sourceMap.SourceMapConsumer
+const SourceNode = sourceMap.SourceNode;
+const SourceMapConsumer = sourceMap.SourceMapConsumer;
 
 module.exports = function(content, map) {
-  this.cacheable()
-
-  var sourceNode
-  var id = this.options.name
+  let sourceNode;
+  const id = this.options.name;
 
   if (!id) {
-    return this.callback(null, content, map)
+    return this.callback(null, content, map);
   }
 
   if (map) {
-    sourceNode = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(map))
+    sourceNode = SourceNode.fromStringWithSourceMap(
+      content,
+      new SourceMapConsumer(map)
+    );
   } else {
-    var fileName = loaderUtils.getRemainingRequest(this)
+    const fileName = loaderUtils.getRemainingRequest(this);
 
-    sourceNode = new SourceNode(null, null, null)
-    content.split('\n').forEach(function(line, idx) {
-      sourceNode.add(new SourceNode(idx + 1, 0, fileName, `${line}\n`))
-    })
-    sourceNode.setSourceContent(fileName, content)
+    sourceNode = new SourceNode(null, null, null);
+    content.split('\n').forEach((line, idx) => {
+      sourceNode.add(new SourceNode(idx + 1, 0, fileName, `${line}\n`));
+    });
+    sourceNode.setSourceContent(fileName, content);
   }
 
-  var concatSrc = new SourceNode()
+  const concatSrc = new SourceNode();
 
   concatSrc.add([
-    `describe(${JSON.stringify(id)}, function() {\n`, sourceNode, '\n});'
-  ])
+    `describe(${JSON.stringify(id)}, function() {\n`,
+    sourceNode,
+    '\n});',
+  ]);
 
-  var result = concatSrc.toStringWithSourceMap()
+  const result = concatSrc.toStringWithSourceMap();
 
-  this.callback(undefined, result.code, result.map.toString())
-}
+  return this.callback(null, result.code, result.map.toString());
+};
