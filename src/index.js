752a5e514c984833854d703abf40f89622c2e30b
Ryan Clark <ryan@ryanclark.me>
2018-09-01 03:40:51 +0100

    refactor(package): apply `webpack-defaults` (#340)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-/*
-	MIT License http://www.opensource.org/licenses/mit-license.php
-	Author Tobias Koppers @sokra
-*/
-module.exports = require('./karma-webpack')
+module.exports = require('./karma-webpack');
