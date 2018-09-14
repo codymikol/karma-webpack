407d1fa8d3de5f43f60a9dd6eb06bcc08b79f1f8
Alabbas Alhaj Ali <alabbas.ali@acomodeo.com>
2018-09-14 11:27:50 +0200

    :bug: fix TypeError: Path must be a string. Received Array

--- src/karma-webpack.js
+++ src/karma-webpack.js
@@ -237,7 +237,11 @@ Plugin.prototype.readFile = function(file, callback) {
   var doRead = function() {
     if (optionsCount > 1) {
       async.times(optionsCount, function(idx, callback) {
-        middleware.fileSystem.readFile(path.join(os.tmpdir(), '_karma_webpack_', String(idx), this.outputs[file]), callback)
+        if (Array.isArray(this.outputs[file])) {
+          middleware.fileSystem.readFile(path.join(os.tmpdir(), '_karma_webpack_', String(idx), this.outputs[file][0]), callback);
+        }else{
+          middleware.fileSystem.readFile(path.join(os.tmpdir(), '_karma_webpack_', String(idx), this.outputs[file]), callback);
+        }
       }.bind(this), function(err, contents) {
         if (err) {
           return callback(err)
@@ -254,7 +258,11 @@ Plugin.prototype.readFile = function(file, callback) {
       })
     } else {
       try {
-        var fileContents = middleware.fileSystem.readFileSync(path.join(os.tmpdir(), '_karma_webpack_', this.outputs[file]))
+        if (Array.isArray(this.outputs[file])) {
+          var fileContents = middleware.fileSystem.readFileSync(path.join(os.tmpdir(), '_karma_webpack_', this.outputs[file][0]));
+        } else {
+          var fileContents = middleware.fileSystem.readFileSync(path.join(os.tmpdir(), '_karma_webpack_', this.outputs[file]));
+        }
 
         callback(undefined, fileContents)
       } catch (e) {
@@ -297,7 +305,11 @@ function createPreprocesor(/* config.basePath */ basePath, webpackPlugin) {
       }
 
       var outputPath = webpackPlugin.outputs[normalize(filename)]
-      file.path = normalize(path.join(basePath, outputPath))
+      if( Array.isArray(outputPath)){
+        file.path = normalize(path.join(basePath, outputPath[0]));
+      } else {
+        file.path = normalize(path.join(basePath, outputPath));
+      }
 
       done(err, content && content.toString())
     })
