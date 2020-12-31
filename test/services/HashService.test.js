491b6f338fcf87752b40c065e0677ef349835a27
Cody Mikol <cody.mikol@boltstech.com>
2020-12-31 13:37:17 -0500

    feat(progresss): getting repo out
    for my own testing purposes
    
    Fixes N/A

---
+++ test/services/HashService.test.js
@@ -0,0 +1,7 @@
+const HashService = require('../../lib/services/HashService');
+
+describe('HashService', () => {
+  it('should be defined', () => {
+    expect(HashService).toBeDefined();
+  });
+});
