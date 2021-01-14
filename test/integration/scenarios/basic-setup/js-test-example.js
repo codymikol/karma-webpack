00e3855daead305e02d0ebcbcc9fb8643f330e07
Cody Mikol <cody.mikol@boltstech.com>
2021-01-14 17:49:41 -0500

    feat(ci): testing github actions
    ...
    
    Fixes N/A

--- test/integration/scenarios/basic-setup/js-test-example.js
+++ test/integration/scenarios/basic-setup/js-test-example.js
@@ -1,24 +1,23 @@
+/* eslint-disable no-undef */
 import AdvancedMath from './js-source-example';
-
-const assert = require('assert');
 
 describe('test one example', () => {
   it('an example passing test', () => {
-    assert.strictEqual(1, 1);
+    assert.equal(1, 1);
   });
 });
 
 describe('test two example', () => {
   it('an example failing test', () => {
-    assert.strictEqual(1, 0);
+    assert.equal(1, 0);
   });
 });
 
 describe('bundled js content', () => {
   it('should be able to bundle js content', () => {
-    assert.strictEqual(!!AdvancedMath.add, true);
+    assert.equal(!!AdvancedMath.add, true);
   });
   it('should be able to interpret bundled js content', () => {
-    assert.strictEqual(AdvancedMath.add(1, 1), 2);
+    assert.equal(AdvancedMath.add(1, 1), 2);
   });
 });
--- test/integration/utils/ScenarioUtils.js
+++ test/integration/utils/ScenarioUtils.js
@@ -1,26 +1,49 @@
-const { fork } = require('child_process');
-const path = require('path');
-
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-console */
 const karma = require('karma');
 
 const ScenarioUtils = { run };
 
 /**
- * This allows you to run a karma with a given configuration and list of plugins,
- * on completion you will be returned a karma results object.
- *
+ * This allows you to run karma with a given configuration and be returned.
+ * either the results of the run, or an error.
  * @param {karma.ConfigOptions} config - The base karma configuration.
- * @param {Array<String>} plugins - A list of plugins to be required
  * @returns {Promise<karma.TestResults>}
  */
-function run(config, plugins) {
+function run(config) {
   return new Promise((resolve, reject) => {
-    fork(path.resolve(`${__dirname}/KarmaWorker.js`))
-      .on('close', reject)
-      .on('error', reject)
-      .on('message', resolve)
-      .send({ config, plugins });
+    const server = new karma.Server(config, (exitCode) => {
+      // todo(mikol): handle karma specific error codes...
+      if (exitCode !== 0) {
+        reject(new Error(`Karma failed with exit code: ${exitCode}`));
+      }
+    });
+
+    server.on('browser_register', (browser) =>
+      console.log(`Karma registered browser: ${browser.name}`)
+    );
+
+    server.on('browser_error', (browser, error) =>
+      console.error(
+        `Karma encountered and error in browser: ${browser.name} - ${error}`
+      )
+    );
+
+    server.on('browser_start', (browser) =>
+      console.log(`Karma started browser: ${browser.name}`)
+    );
+
+    server.on(
+      'browser_complete',
+      (browser) => `Karma browser: ${browser.name} completed`
+    );
+
+    server.on('browsers_ready', () => {
+      console.log('Karma reports all browsers are ready');
+    });
+
+    server.on('run_complete', (browsers, results) => resolve(results));
+
+    server.start();
   });
 }
 
