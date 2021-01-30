ea5dc8ec56ac2752f76e1b1858bf1320ff2ee127
Cody Mikol <codymikol@gmail.com>
2021-01-30 01:59:21 -0500

    fix(preprocess): auto fix missing webpack framework (#471)
    this is an issue that comes up often. As this is
    something that karma-webpack needs to function,
    we will automatically fix the configuration on
    the fly and emit a warning that the change was
    made.
    
    Fixes N/A

---
+++ test/unit/util/karmaConfigValidation.test.js
@@ -0,0 +1,29 @@
+const {
+  ensureWebpackFrameworkSet,
+} = require('../../../lib/karma/karmaConfigValidator');
+
+describe('karmaConfigValidation', () => {
+  describe('ensureWebpackFrameworkExists', () => {
+    let config;
+    beforeEach(() => (config = { frameworks: [] }));
+
+    it('should add webpack to the list of karma config frameworks if it did not already exist', () => {
+      ensureWebpackFrameworkSet(config);
+      expect(config.frameworks.length).toBe(1);
+      expect(config.frameworks[0]).toBe('webpack');
+    });
+
+    it('should add webpack to an existing list of frameworks', () => {
+      config.frameworks = ['foo', 'bar'];
+      ensureWebpackFrameworkSet(config);
+      expect(config.frameworks.length).toBe(3);
+      expect(config.frameworks).toContain('webpack');
+    });
+    it('should create a frameworks array if one does not exist', () => {
+      delete config.frameworks;
+      ensureWebpackFrameworkSet(config);
+      expect(config.frameworks.length).toBe(1);
+      expect(config.frameworks[0]).toBe('webpack');
+    });
+  });
+});
